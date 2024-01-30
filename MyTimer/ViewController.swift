4d434445932a0261088ac567131b915da9f3deea
gadgelogger <sarayou273142910@gmail.com>
2024-01-31 01:42:03 +0900

    Initial Commit

---
+++ MyTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTimer
+//
+//  Created by gadgelogger on 2024/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
